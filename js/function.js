178c815459eb06ee1e01053f5c83f15a92302582
nicwitt <106807820+nicwitt@users.noreply.github.com>
2022-06-03 20:23:05 +1000

    Add files via upload

---
+++ js/function.js
@@ -0,0 +1,23 @@
+// ----------------NAV BAR------------------
+
+
+// ----------------SIDE NAV------------------
+
+function openNav() {
+  document.getElementById("mySidenav").style.width = "40vw";
+  document.body.style.backgroundColor = "#rgba(0,0,0,0.4)";
+}
+
+function closeNav() {
+  document.getElementById("mySidenav").style.width = "0";
+  document.body.style.backgroundColor = "white";
+}
+
+// ----------------FILTER BUTTONS------------------
+
+
+// ----------------ADD TO CART------------------
+
+
+
+// ----------------CHECKOUT VALIDATION------------------
